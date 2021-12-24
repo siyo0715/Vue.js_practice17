e8bd25bd29904fb68e17f40ed4667d5251b8da31
向山　翔 <hms0175km@gmail.com>
2021-12-24 15:27:08 +0900

    [Add]

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
+
+
+Vue.component('my-component', {
+    data: function () {
+        return {
+            number: 0
+        }
+    },
+    template: '<p>いいね({{number}})<button @click="increment"> +1</button></p>',
+    methods: {
+        increment: function () {
+            this.number += 1;
+      }
+    }
+})
+
 new Vue({
     el: '#app',
-    data: {
-        number:12
-    }
 })
