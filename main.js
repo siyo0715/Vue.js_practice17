290fc7dacad20ddf7b38c352641c159ae4e83919
向山　翔 <hms0175km@gmail.com>
2021-12-24 15:36:54 +0900

    [Finish]Vue.js_practice17

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-const data = {
-    number:0
-}
-
 Vue.component('my-component', {
     data: function(){
-        return data
+        return {
+            number: 0
+        }
     },
     template: '<p>いいね({{number}})<button @click="increment"> +1</button></p>',
     methods: {
